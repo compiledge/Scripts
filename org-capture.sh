d23a9b9f1b59c8e26fecc4d1004480a16674da1d
compiledge <eduardo.iftm@gmail.com>
2023-09-24 21:06:27 -0300

    feat: Add org-mode catpure script
    This script is based in orgmode capture function and is used to catpure
    ideas outside the text editor. Better used with a rapid keybind.

---
+++ org-capture.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+nivel='*'
+todo="NEXT"
+sdate=$(date +%Y-%m-%d' '%a)
+note=$(zenity --title="Org Capture" --text "💡 Any idea?" --width=600 --entry)
+
+# Avoid empty insertions
+if [ -n "$note" ]; then
+	task="$nivel $todo $note"
+	echo "$task" >> ~/git/org/refile.org
+	
+	# Include the current date on orgmode format
+	echo "  [$sdate]" >> ~/git/org/refile.org
+fi
