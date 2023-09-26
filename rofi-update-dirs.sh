7e7a21496ea081b8907f649862c8ad69fb91e8df
compiledge <eduardo.iftm@gmail.com>
2023-09-26 15:15:59 -0300

    fix: Overwrite dir database and ignore hidden dirs
    The '>>' operator was substituted to '>' operator, overwriting the
    directories database of the rofi-update script. Also, the hidden files
    were changed to be ignored.

--- rofi-update-dirs.sh
+++ rofi-update-dirs.sh
@@ -3,14 +3,14 @@
 echo "[info]: Reading the directories list."
 
 # INFO: The -type flag specifies directories
-dirList=$(find $HOME -path '*/.*' -type d)
+# dirList=$(find $HOME -path '*/.*' -type d)
 
 # INFO: With the -not flag, find ignores hidden directories
 # WARN: Without this, find can produce a much larger file
-# dirList=$(find $HOME -not -path '*/.*' -type d)
+dirList=$(find $HOME -not -path '*/.*' -type d)
 
 echo "[info]: Saving the directories list."
-echo "$dirList" >> $HOME/.config/rofi/dir-db.txt
+echo "$dirList" > $HOME/.config/rofi/dir-db.txt
 
 # The list directories database (dir-db) should
 # be read int the rofi-read-dirs.sh scritp.
