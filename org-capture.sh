648761ff24796e39fc2931ec262f2fc75fc4f953
compiledge <eduardo.iftm@gmail.com>
2023-11-29 23:13:20 -0300

    feat: Add a timestamp option in the org-capture

--- org-capture.sh
+++ org-capture.sh
@@ -4,15 +4,48 @@ nivel='*'
 todo="NEXT"
 sdate=$(date +%Y-%m-%d' '%a)
 note=$(zenity --title="Org Capture" --text "💡 Any idea?" --width=600 --entry)
+tstamp=false
+
+# Capturing arguments
+while getopts "th" opt; do
+	case $opt in
+	t)
+		tstamp=true
+		;;
+	h)
+		echo -e "Usage: ./org-capture.sh [opts]\n"
+		echo -e "Opts:"
+		echo -e "\t -t \t\t enable timestamp"
+		echo -e "\t -h \t\t print this menu"
+		echo -e "\nExample:"
+		echo -e "\t ./org-capture.sh -t "
+		exit 1
+		;;
+
+	\?)
+		echo "Invalid option -$OPTARG" >&2
+		exit 1
+		;;
+	esac
+
+	case $OPTARG in
+	-*)
+		echo "Option $opt needs a valid argument"
+		exit 1
+		;;
+	esac
+done
 
 # Avoid empty insertions
 if [ -n "$note" ]; then
 	task="$nivel $todo $note"
-	echo "$task" >> ~/git/org/refile.org
-	
+	echo "$task" >>~/git/org/refile.org
+
 	# Include the current date on orgmode format
-	# echo "  [$sdate]" >> ~/git/org/refile.org
+	if [ "$tstamp" = true ]; then
+		echo "  [$sdate]" >>~/git/org/refile.org
+	fi
 
 	# Breakline to separate headers
-	echo "" >> ~/git/org/refile.org
+	echo "" >>~/git/org/refile.org
 fi
